6122b13d064f19c69b593b2c2699153a098877e0
zhaoyang110157 <1853935753@qq.com>
2019-05-04 20:47:29 +0800

    SpringBoot Trial 5.4

---
+++ src/main/java/com/example/spring/ebook/controller/HiController.java
@@ -0,0 +1,12 @@
+package com.example.spring.ebook.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller // pay attention here
+public class HiController {
+    @RequestMapping("/hi")
+        public String hi(){
+        return "index";
+    }
+}
